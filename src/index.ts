17da5957f2d137e64747691c8c4ecebdb80bdc76
Ninad <m.ninad99@gmail.com>
2022-01-13 18:48:02 +0530

    feat: initial commit - repo setup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,44 @@
+import { NextFunction, Request, Response } from 'express';
 
-function test() {
-    return 'Happy codding';
+function getMiddleware({
+    dynamicHeaders = {},
+    staticHeaders = {},
+    copyFromRequestHeaders = [],
+}: IConfig = {}) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const copiedFromRequestHeaders: any = {};
+        for (const header of copyFromRequestHeaders) {
+            copiedFromRequestHeaders[header] = req.get(header);
+        }
+        const dynamicResolvedHeaders: any = {};
+        for (const header in dynamicHeaders) {
+            if (typeof dynamicHeaders[header] === 'function') {
+                dynamicResolvedHeaders[header] = dynamicHeaders[header](req);
+            }
+        }
+        const combinedHeaders = Object.assign(
+            {},
+            staticHeaders,
+            copiedFromRequestHeaders,
+            dynamicResolvedHeaders,
+        );
+        res.set(combinedHeaders);
+        next();
+    };
 }
 
-export default test;
+export interface IConfig {
+    staticHeaders?: IStaticValues;
+    dynamicHeaders?: IDynamicValues;
+    copyFromRequestHeaders?: string[];
+}
+
+interface IStaticValues {
+    [key: string]: string;
+}
+
+interface IDynamicValues {
+    [key: string]: (arg0: Request) => string;
+}
+
+export default getMiddleware;
